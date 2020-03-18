1fa19f7d599d2ae645ff5a323e894bb704d9eba7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-18 06:53:54 +0000

    Add dialogue component for multiple messages

---
+++ Runtime/TextboxDialogue.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextboxDialogue : MonoBehaviour
+{
+    [SerializeField]
+    [Multiline]
+    private string[] messages;
+
+    [SerializeField]
+    private bool playOnStart = true;
+
+    [SerializeField]
+    private Textbox textbox;
+    private Textbox Textbox => this.LazyGet(ref textbox);
+
+    [SerializeField]
+    private TextboxPrompt prompt;
+    private TextboxPrompt Prompt => this.LazyGet(ref prompt);
+
+    [SerializeField]
+    private bool automatic = false;
+    [SerializeField]
+    private float automaticDelay = 0.0f;
+    [SerializeField]
+    private bool useScaledTime = true;
+
+    [SerializeField]
+    private UnityEvent dialogueStart = default;
+    public UnityEvent DialogueStart => dialogueStart;
+
+    [SerializeField]
+    private UnityEvent dialogueEnd = default;
+    public UnityEvent DialogueEnd => dialogueEnd;
+
+    private void Start()
+    {
+        if (playOnStart) StartDialogue();
+    }
+
+    public void StartDialogue()
+    {
+        StartCoroutine(Dialogue());
+    }
+
+    private IEnumerator Dialogue()
+    {
+        dialogueStart.Invoke();
+
+        foreach (string message in messages)
+        {
+            if (automatic)
+            {
+                yield return Textbox.Text(message);
+                yield return YieldUtil.WaitForSecondsScaled(automaticDelay, useScaledTime);
+            }
+            else
+            {
+                yield return Prompt.TextAndWait(message);
+            }
+        }
+
+        dialogueEnd.Invoke();
+    }
+}
