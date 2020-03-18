e6f7ba1d5e814f3ac80c6f8079486926b2d99ec2
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-18 06:20:01 +0000

    Add a key prompt with coroutines to prompt text

---
+++ Runtime/TextboxPrompt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TextboxPrompt : MonoBehaviour
+{
+    [SerializeField]
+    private Textbox textbox;
+    public Textbox Textbox => this.LazyGet(ref textbox);
+
+    [SerializeField]
+    private string buttonName = default;
+
+    public IEnumerator Wait()
+    {
+        while (Textbox.Typing || !Input.GetButton(buttonName))
+        {
+            yield return null;
+        }
+    }
+
+    public IEnumerator TextAndWait(string richText)
+    {
+        yield return Textbox.Text(richText);
+        yield return Wait();
+    }
+}
