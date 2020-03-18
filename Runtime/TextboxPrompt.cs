0e391ae31bcf7a752673e875fb2d2afcb1c08ce1
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-03-18 17:29:52 +0000

    Add turbo option to prompt

--- Runtime/TextboxPrompt.cs
+++ Runtime/TextboxPrompt.cs
@@ -10,17 +10,44 @@ public class TextboxPrompt : MonoBehaviour
     [SerializeField]
     private string buttonName = "Submit";
 
+    [SerializeField]
+    private float turboDelayScale = 1.0f;
+
     public IEnumerator Wait()
     {
-        while (Textbox.Typing || !Input.GetButton(buttonName))
+        Textbox.delayScale = 1.0f;
+
+        while (true)
         {
-            yield return null;
+            if (Input.GetButtonDown(buttonName))
+            {
+                if (Textbox.Typing)
+                {
+                    Textbox.delayScale = turboDelayScale;
+                    yield return null;
+                }
+                else
+                {
+                    Textbox.delayScale = 1.0f;
+                    yield break;
+                }
+            }
+            else
+            {
+                yield return null;
+            }
         }
     }
 
     public IEnumerator TextAndWait(string richText)
     {
-        yield return Textbox.Text(richText);
-        yield return Wait();
+        Coroutine text = StartCoroutine(Textbox.Text(richText));
+
+        // Start waiting 1 frame later to avoid double button press
+        yield return null;
+        Coroutine wait = StartCoroutine(Wait());
+
+        yield return text;
+        yield return wait;
     }
 }
